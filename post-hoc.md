244c64f420ad60fa84e4eb03b83d6c0f37e9adb1
pannous <info@pannous.com>
2026-01-10 21:53:06 +0100

    feat(netutils): add ping build script for Cranelift
    - Build ping with proper Cranelift flags (-z muldefs for allocator)
    - Installed to image at /usr/bin/ping
    - Tested: ping pannous.com works from Redox

---
+++ post-hoc.md
@@ -0,0 +1,7 @@
+
+## 2026-01-10 - ping installed
+- Built ping from recipes/core/netutils with Cranelift
+- Installed to /usr/bin/ping in image
+- Source at recipes/core/base/source/target/aarch64-unknown-redox-clif/release/ping
+- Build script at recipes/core/netutils/build-ping.sh
+
---
+++ recipes/core/netutils/build-ping.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+cd "$(dirname "$0")/source"
+
+NIGHTLY="nightly-2026-01-02"
+TARGET="/opt/other/redox/recipes/core/base/source/aarch64-unknown-redox-clif.json"
+CRANELIFT="/opt/other/rustc_codegen_cranelift/dist/lib/librustc_codegen_cranelift.dylib"
+RELIBC="/opt/other/redox/recipes/core/relibc/source/target/aarch64-unknown-redox-clif/release"
+
+export DYLD_LIBRARY_PATH=~/.rustup/toolchains/${NIGHTLY}-aarch64-apple-darwin/lib
+export CARGO_INCREMENTAL=0
+export RUSTC_WRAPPER=""
+
+export RUSTFLAGS="-Zcodegen-backend=${CRANELIFT} \
+  -Crelocation-model=static \
+  -Clink-arg=-L${RELIBC} \
+  -Clink-arg=${RELIBC}/crt0.o \
+  -Clink-arg=${RELIBC}/crti.o \
+  -Clink-arg=${RELIBC}/crtn.o \
+  -Clink-arg=-lunwind_stubs \
+  -Clink-arg=-z -Clink-arg=muldefs \
+  -Cpanic=abort"
+
+cargo +${NIGHTLY} build \
+    --target ${TARGET} \
+    --release \
+    -Z build-std=core,alloc,std,panic_abort \
+    -Zbuild-std-features=compiler_builtins/no-f16-f128 \
+    --bin ping
+
+echo "=== Stripping ==="
+llvm-strip -o /opt/other/redox/share/ping target/aarch64-unknown-redox-clif/release/ping
+ls -la /opt/other/redox/share/ping
+echo "Done - ping is at /opt/other/redox/share/ping"
